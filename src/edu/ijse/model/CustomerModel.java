b9189363dffe916565de16c6da26d607bb35cf95
Shehan Nethsara <Shehan.nethsara2003@gmail.com>
2024-09-11 13:12:54 +0530

    update,delete customer Model

--- src/edu/ijse/model/CustomerModel.java
+++ src/edu/ijse/model/CustomerModel.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package edu.ijse.model;
 
 import edu.ijse.dto.CustomerDto;
@@ -11,35 +8,28 @@ import java.sql.PreparedStatement;
 import edu.ijse.db.DBConnection;
 
 
-
-
-
-
-/**
- *
- * @author user
- */
 public class CustomerModel {
     
     
     
     public String saveCustomr(CustomerDto customerDto) throws SQLException, ClassNotFoundException{
         Connection connection = DBConnection.getInstance().getConnection();
-        String sql = "INSERT INTO Customer VALUES (?,?,?,?,?,?,?,?,?)";
+        String sql = "Update  Customer SET CustTitle = ? ,CusName = ?,DOB = ?,salary = ?,CustAddress = ?, City = ?, Province =?, PostalCode =? WHERE CustID";
         PreparedStatement statement = connection.prepareStatement(sql);
 
-       statement.setString(1, customerDto.getId());
-       statement.setString(2, customerDto.getTitle());
-       statement.setString(3, customerDto.getName());
-       statement.setString(4, customerDto.getDob());
-       statement.setDouble(5, customerDto.getSalary());
-       statement.setString(6, customerDto.getAddress());
-       statement.setString(7, customerDto.getCity());
-       statement.setString(8, customerDto.getProvince());
-       statement.setString(9, customerDto.getPostalCode());
+      
+       statement.setString(1, customerDto.getTitle());
+       statement.setString(2, customerDto.getName());
+       statement.setString(3, customerDto.getDob());
+       statement.setDouble(4, customerDto.getSalary());
+       statement.setString(5, customerDto.getAddress());
+       statement.setString(6, customerDto.getCity());
+       statement.setString(7, customerDto.getProvince());
+       statement.setString(8, customerDto.getPostalCode());
+       statement.setString(9, customerDto.getId());
        
        int result = statement.executeUpdate();
-       return result > 0? "Successfully" : "Fail";
+       return result > 0? "Successfully Update" : "Fail";
         
     }
     
